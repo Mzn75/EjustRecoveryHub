7c660453e23cc9e98009b984727d810c3cfb200f
mazee <mazee@MZ_75-PC>
2026-07-13 10:32:54 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EjustRecoveryHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
